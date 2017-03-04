8b9680ad1fa3c8ad319719e63a4684df8956fea6
stephanernst <stephan_ernst@live.com>
2017-03-04 17:40:11 -0500

    Added @EnableWebSecurity

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,8 +6,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Application {
 
     public static void main(String[] args) throws Throwable {
--- src/main/java/hello/MvcConfiguration.java
+++ src/main/java/hello/MvcConfiguration.java
@@ -1,12 +1,14 @@
 package hello;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by steph on 2017-03-04.
  */
-public class Configuration extends WebMvcConfigurerAdapter {
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
