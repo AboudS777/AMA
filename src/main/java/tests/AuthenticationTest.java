843221876fb39a24c34321f45b7e79e23783b0b3
SarrankanPathmanathan <sarran_01@hotmail.ca>
2017-03-09 12:03:22 -0500

    Added Unit Tests for Authentication

--- src/main/java/tests/AuthenticationTest.java
+++ src/main/java/tests/AuthenticationTest.java
@@ -7,7 +7,6 @@ import static org.hamcrest.Matchers.equalTo;
 import java.net.URL;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
