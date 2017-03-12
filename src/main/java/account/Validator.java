813bf9a93b179191020a1fcafe07b976c85047f4
SarrankanPathmanathan <sarran_01@hotmail.ca>
2017-03-12 18:39:35 -0400

    Check for password length

--- src/main/java/account/Validator.java
+++ src/main/java/account/Validator.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
-/**
- * Created by Stephane on 2017-03-07.
- */
-
 @Service
 public class Validator {
 
@@ -18,6 +14,9 @@ public class Validator {
     public void validate(User user, BindingResult result) {
         if (user.getUsername().length() < 2 || user.getUsername().length() > 20) {
             result.rejectValue("username", "Size.user.username");
+        }
+        if (user.getPassword().length() < 2 || user.getPassword().length() > 20) {
+            result.rejectValue("password", "Size.user.password");
         }
         if (userService.loadUserByUsername(user.getUsername()) != null) {
             result.rejectValue("username", "Duplicate.user.username");
