94ab3af8fcf8f9e81560fea758681be6de2aee01
stephanernst <stephan_ernst@live.com>
2017-03-19 14:54:17 -0400

    Added error display for registration page. -username and password length -username already taken

--- src/main/java/account/Validator.java
+++ src/main/java/account/Validator.java
@@ -13,13 +13,13 @@ public class Validator {
 
     public void validate(User user, BindingResult result) {
         if (user.getUsername().length() < 2 || user.getUsername().length() > 20) {
-            result.rejectValue("username", "Size.user.username");
+            result.rejectValue("username", "Size.user.username", "Username must be between 2 and 20 characters.");
         }
         if (user.getPassword().length() < 2 || user.getPassword().length() > 20) {
-            result.rejectValue("password", "Size.user.password");
+            result.rejectValue("password", "Size.user.password", "Password must be between 2 and 20 characters.");
         }
         if (userService.loadUserByUsername(user.getUsername()) != null) {
-            result.rejectValue("username", "Duplicate.user.username");
+            result.rejectValue("username", "Duplicate.user.username", "Username is already taken.");
         }
     }
 }
