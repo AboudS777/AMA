344defa09d6d0eb901635ef03ed22c8c4a93b528
SarrankanPathmanathan <sarran_01@hotmail.ca>
2017-03-28 17:13:38 -0400

    Unit Tests for Entities and Repositories

---
+++ src/main/java/tests/UnitTests/TestSubmissionPostRepository.java
@@ -0,0 +1,52 @@
+package tests.UnitTests;
+
+import ama.Application;
+import ama.account.User;
+import ama.account.UserRepository;
+import ama.post.SubmissionPost;
+import ama.post.SubmissionPostRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TestSubmissionPostRepository {
+
+    @Autowired
+    private SubmissionPostRepository submissionPostRepository;
+
+    private SubmissionPost submissionPost;
+
+    @Before
+    public void setUp() throws Exception {
+        submissionPost = new SubmissionPost();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        submissionPost = null;
+    }
+
+    @Test
+    public void testCommentPost() throws Exception {
+        assertNotNull(submissionPost);
+    }
+
+    @Test
+    public void testFindPostByTitle() throws Exception{
+        submissionPost.setTitle("Title");
+        submissionPost.setText("Description");
+        submissionPostRepository.save(submissionPost);
+        SubmissionPost foundPost = submissionPostRepository.findByTitle(submissionPost.getTitle());
+        assertEquals(foundPost.getTitle(),submissionPost.getTitle());
+        assertEquals(foundPost.getText(),submissionPost.getText());
+    }
+}
---
+++ src/main/java/tests/UnitTests/TestUserService.java
@@ -0,0 +1,59 @@
+package tests.UnitTests;
+
+import ama.Application;
+import ama.account.User;
+import ama.account.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TestUserService {
+
+    @Autowired
+    private UserService userService;
+
+    private User ryan;
+    private User sarran;
+    @Before
+    public void setUp() throws Exception {
+        ryan = new User("ryan","theman");
+        sarran = new User("sarran","theman");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ryan = null;
+    }
+
+    @Test
+    public void testRegisterNewUserAccount() throws Exception {
+        User registeredUser = userService.registerNewUserAccount(sarran);
+        assertEquals(registeredUser.getUsername(),sarran.getUsername());
+        assertEquals(registeredUser.getPassword(),sarran.getPassword());
+    }
+
+    @Test(expected= NullPointerException.class)
+    public void testLoadUserThatDoesNotExistByUsername() throws Exception {
+        UserDetails loadedUser = userService.loadUserByUsername(ryan.getUsername());
+        assertEquals(loadedUser.getUsername(),ryan.getUsername());
+    }
+    @Test
+    public void testLoadUserThatExistsByUsername() throws Exception {
+        userService.registerNewUserAccount(ryan);
+        UserDetails loadedUser = userService.loadUserByUsername(ryan.getUsername());
+        assertEquals(loadedUser.getUsername(),ryan.getUsername());
+    }
+
+}
